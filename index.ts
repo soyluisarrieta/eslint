562abc8879cc15f6c9a143587885fc255322ed96
soyluisarrieta <luisarrieta796@gmail.com>
2025-04-06 16:31:20 -0500

    Migrate configuration to flat config format

--- index.ts
+++ index.ts
@@ -1,59 +1,65 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  plugins: ['react', '@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  rules: {
-    '@typescript-eslint/no-unused-expressions': 'off',
-    '@typescript-eslint/no-misused-promises': 'off',
-    '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    'semi': ['error', 'never'],
-    'quotes': ['error', 'single'],
-    'space-before-function-paren': ['error', 'always'],
-    'comma-dangle': ['error', 'never'],
-    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
-    'eol-last': ['error', 'always'],
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'never'],
-    'computed-property-spacing': ['error', 'never'],
-    'space-in-parens': ['error', 'never'],
-    'space-before-blocks': ['error', 'always'],
-    'keyword-spacing': ['error', { 'before': true, 'after': true }],
-    'space-infix-ops': 'error',
-    'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }],
-    'indent': ['error', 2],
-    'no-trailing-spaces': 'error',
-    'react/jsx-indent': ['error', 2],
-    'react/react-in-jsx-scope': 'off',
-    'arrow-spacing': ['error', { 'before': true, 'after': true }],
-    'react/jsx-tag-spacing': [
-      'error',
-      {
-        'closingSlash': 'never',
-        'beforeSelfClosing': 'always',
-        'afterOpening': 'never',
-        'beforeClosing': 'never',
+const js = require('@eslint/js');
+const react = require('eslint-plugin-react');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
       },
-    ],
+    },
+    plugins: {
+      react,
+      '@typescript-eslint': typescriptEslint,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single'],
+      'space-before-function-paren': ['error', 'always'],
+      'comma-dangle': ['error', 'never'],
+      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
+      'eol-last': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'computed-property-spacing': ['error', 'never'],
+      'space-in-parens': ['error', 'never'],
+      'space-before-blocks': ['error', 'always'],
+      'keyword-spacing': ['error', { 'before': true, 'after': true }],
+      'space-infix-ops': 'error',
+      'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }],
+      'indent': ['error', 2],
+      'no-trailing-spaces': 'error',
+      'react/jsx-indent': ['error', 2],
+      'react/react-in-jsx-scope': 'off',
+      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'react/jsx-tag-spacing': [
+        'error',
+        {
+          'closingSlash': 'never',
+          'beforeSelfClosing': 'always',
+          'afterOpening': 'never',
+          'beforeClosing': 'never',
+        },
+      ],
+    },
   },
-};
+];
